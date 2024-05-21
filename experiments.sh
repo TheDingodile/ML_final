d912a561c7e3422b8d5254240e64c4baffe19bd9
TheDingodile <jakob@3esser.dk>
2024-05-21 13:19:04 +0900

    123

--- experiments.sh
+++ experiments.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mkdir -p outputs/Test1/Markdown
-bsub -o "outputs/Test1/Markdown/Test1_0.md" -J "Test1_0" -env MYARGS="-name Test1-0 -time 3600 -ID 0" < submit_cpu.sh
+mkdir -p outputs/Test1GPU/Markdown
+bsub -o "outputs/Test1GPU/Markdown/Test1GPU_0.md" -J "Test1GPU_0" -env MYARGS="-name Test1GPU-0 -time 3600 -ID 0" < submit_gpu_v32.sh
