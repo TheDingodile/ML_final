da20d641f159bd2495d7ba81554ce213005854dc
TheDingodile <jakob@3esser.dk>
2024-05-22 18:18:44 +0900

    123

--- experiments.sh
+++ experiments.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mkdir -p outputs/CrashTester/Markdown
-bsub -o "outputs/CrashTester/Markdown/CrashTester_0.md" -J "CrashTester_0" -env MYARGS="-name CrashTester-0 -time 3600 -lr 0.03 -batch_size 8 -steps 1000 -ID 0" < submit_cpu.sh
+bsub -o "outputs/CrashTester/Markdown/CrashTester_0.md" -J "CrashTester_0" -env MYARGS="-name CrashTester-0 -time 3600 -lr 0.03 -batch_size 2 -steps 1000 -ID 0" < submit_cpu.sh
