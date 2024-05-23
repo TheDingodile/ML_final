9aadfb6e27bd6f0beb195c2b20ad66f861ba0fcd
TheDingodile <jakob@3esser.dk>
2024-05-23 15:49:28 +0900

    123

--- experiments.sh
+++ experiments.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-mkdir -p outputs/Cuda_version_check2/Markdown
-bsub -o "outputs/Cuda_version_check2/Markdown/Cuda_version_check2_0.md" -J "Cuda_version_check2_0" -env MYARGS="-name Cuda_version_check2-0 -time 3600 -lr 0.03 -batch_size 8 -steps 1000 -ID 0" < submit_cpu.sh
+mkdir -p outputs/GPU_check/Markdown
+bsub -o "outputs/GPU_check/Markdown/GPU_check_0.md" -J "GPU_check_0" -env MYARGS="-name GPU_check-0 -time 3600 -lr 0.03 -batch_size 8 -steps 1000 -ID 0" < submit_gpu_v32.sh
+mkdir -p outputs/GPU_check2/Markdown
+bsub -o "outputs/GPU_check2/Markdown/GPU_check2_0.md" -J "GPU_check2_0" -env MYARGS="-name GPU_check2-0 -time 3600 -lr 0.03 -batch_size 8 -steps 1000 -ID 0" < submit_gpu_a80.sh
--- main.py
+++ main.py
@@ -24,12 +24,6 @@ class Defaults(Parameters):
 
 
     def run(self, name: str, isServer: bool, lr: float, batch_size: int, steps: int) -> None:
-        cuda_available = torch.cuda.is_available()
-        cuda_version = torch.version.cuda if cuda_available else "No CUDA available"
-        print(f"CUDA available: {cuda_available}")
-        print(f"CUDA version: {cuda_version}")
-        quit()
-
         # from paligemma import run_test
         from big_vision_test import big_vision_test, update_fn
         from vqa_dataset import VQA_Dataset
