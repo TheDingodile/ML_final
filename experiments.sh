f39406520807bb8c44e642047adc9a41a3c3c7a1
TheDingodile <jakob@3esser.dk>
2024-05-23 15:19:35 +0900

    123

--- experiments.sh
+++ experiments.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mkdir -p outputs/GPU_run/Markdown
-bsub -o "outputs/GPU_run/Markdown/GPU_run_0.md" -J "GPU_run_0" -env MYARGS="-name GPU_run-0 -time 3600 -lr 0.03 -batch_size 8 -steps 1000 -ID 0" < submit_gpu_v32.sh
+mkdir -p outputs/GPU_run_a80/Markdown
+bsub -o "outputs/GPU_run_a80/Markdown/GPU_run_a80_0.md" -J "GPU_run_a80_0" -env MYARGS="-name GPU_run_a80-0 -time 3600 -lr 0.03 -batch_size 8 -steps 1000 -ID 0" < submit_gpu_a80.sh
--- generate.py
+++ generate.py
@@ -3,4 +3,5 @@ from main import Defaults, GPU
 
 # Defaults("Test1CPU", GPU=None)
 # Defaults("CrashTester", GPU=None)
-Defaults("GPU_run", GPU=GPU.v32)
+# Defaults("GPU_run", GPU=GPU.v32)
+Defaults("GPU_run_a80", GPU=GPU.a80)
