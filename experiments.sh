7e1014d2e8f728432954e33031352c913f35c72a
TheDingodile <jakob@3esser.dk>
2024-05-23 16:12:41 +0900

    123

--- experiments.sh
+++ experiments.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mkdir -p outputs/CPU_check/Markdown
-bsub -o "outputs/CPU_check/Markdown/CPU_check_0.md" -J "CPU_check_0" -env MYARGS="-name CPU_check-0 -time 3600 -lr 0.03 -batch_size 8 -steps 1000 -ID 0" < submit_cpu.sh
+mkdir -p outputs/Batch_size/Markdown
+bsub -o "outputs/Batch_size/Markdown/Batch_size_0.md" -J "Batch_size_0" -env MYARGS="-name Batch_size-0 -time 3600 -lr 0.001 -batch_size 64 -steps 5000 -ID 0" < submit_gpu_a80.sh
--- generate.py
+++ generate.py
@@ -2,8 +2,8 @@ from main import Defaults, GPU
 
 
 # Defaults("Test1CPU", GPU=None)
-Defaults("CPU_check", GPU=None)
+# Defaults("CPU_check", GPU=None)
 # Defaults("GPU_check", GPU=GPU.v32)
-# Defaults("GPU_check2", GPU=GPU.a80)
+Defaults("Batch_size", GPU=GPU.a80, batch_size=64, steps=5000, lr=0.001)
 
 # Defaults("Cuda_version_check2", GPU=None)
