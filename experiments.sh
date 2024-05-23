b22a355fb6f4d10ee84e3c3d1549c8847bde4b3e
TheDingodile <jakob@3esser.dk>
2024-05-23 16:15:29 +0900

    123

--- experiments.sh
+++ experiments.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mkdir -p outputs/Batch_size/Markdown
-bsub -o "outputs/Batch_size/Markdown/Batch_size_0.md" -J "Batch_size_0" -env MYARGS="-name Batch_size-0 -time 3600 -lr 0.001 -batch_size 64 -steps 5000 -ID 0" < submit_gpu_a80.sh
+mkdir -p outputs/Batch_size_v32/Markdown
+bsub -o "outputs/Batch_size_v32/Markdown/Batch_size_v32_0.md" -J "Batch_size_v32_0" -env MYARGS="-name Batch_size_v32-0 -time 3600 -lr 0.001 -batch_size 64 -steps 5000 -ID 0" < submit_gpu_v32.sh
