39b98aa7299a47dd6f64a629da845411109a31b0
TheDingodile <jakob@3esser.dk>
2024-05-23 15:28:02 +0900

    123

--- experiments.sh
+++ experiments.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mkdir -p outputs/GPU_run_a80/Markdown
-bsub -o "outputs/GPU_run_a80/Markdown/GPU_run_a80_0.md" -J "GPU_run_a80_0" -env MYARGS="-name GPU_run_a80-0 -time 3600 -lr 0.03 -batch_size 8 -steps 1000 -ID 0" < submit_gpu_a80.sh
+mkdir -p outputs/Cuda_version_check/Markdown
+bsub -o "outputs/Cuda_version_check/Markdown/Cuda_version_check_0.md" -J "Cuda_version_check_0" -env MYARGS="-name Cuda_version_check-0 -time 3600 -lr 0.03 -batch_size 8 -steps 1000 -ID 0" < submit_cpu.sh
