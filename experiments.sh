1d7d637182ccaa268ab2291ebfccf6d2b5c595c5
TheDingodile <jakob@3esser.dk>
2024-05-23 15:52:20 +0900

    123

--- experiments.sh
+++ experiments.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-mkdir -p outputs/GPU_check/Markdown
-bsub -o "outputs/GPU_check/Markdown/GPU_check_0.md" -J "GPU_check_0" -env MYARGS="-name GPU_check-0 -time 3600 -lr 0.03 -batch_size 8 -steps 1000 -ID 0" < submit_gpu_v32.sh
-mkdir -p outputs/GPU_check2/Markdown
-bsub -o "outputs/GPU_check2/Markdown/GPU_check2_0.md" -J "GPU_check2_0" -env MYARGS="-name GPU_check2-0 -time 3600 -lr 0.03 -batch_size 8 -steps 1000 -ID 0" < submit_gpu_a80.sh
+mkdir -p outputs/CPU_check/Markdown
+bsub -o "outputs/CPU_check/Markdown/CPU_check_0.md" -J "CPU_check_0" -env MYARGS="-name CPU_check-0 -time 3600 -lr 0.03 -batch_size 8 -steps 1000 -ID 0" < submit_cpu.sh
