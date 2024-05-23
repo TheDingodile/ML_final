b2f27ffdca841b4231936a24ffa405faed62a1af
TheDingodile <jakob@3esser.dk>
2024-05-23 19:34:31 +0900

    123

--- experiments.sh
+++ experiments.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-mkdir -p outputs/32_001/Markdown
-bsub -o "outputs/32_001/Markdown/32_001_0.md" -J "32_001_0" -env MYARGS="-name 32_001-0 -time 3600 -lr 0.01 -batch_size 32 -steps 5000 -ID 0" < submit_gpu_v32.sh
-mkdir -p outputs/16_001/Markdown
-bsub -o "outputs/16_001/Markdown/16_001_0.md" -J "16_001_0" -env MYARGS="-name 16_001-0 -time 3600 -lr 0.01 -batch_size 16 -steps 5000 -ID 0" < submit_gpu_v32.sh
-mkdir -p outputs/32_01/Markdown
-bsub -o "outputs/32_01/Markdown/32_01_0.md" -J "32_01_0" -env MYARGS="-name 32_01-0 -time 3600 -lr 0.1 -batch_size 32 -steps 5000 -ID 0" < submit_gpu_v32.sh
-mkdir -p outputs/16_01/Markdown
-bsub -o "outputs/16_01/Markdown/16_01_0.md" -J "16_01_0" -env MYARGS="-name 16_01-0 -time 3600 -lr 0.1 -batch_size 16 -steps 5000 -ID 0" < submit_gpu_v32.sh
+mkdir -p outputs/32_001_alltrainable/Markdown
+bsub -o "outputs/32_001_alltrainable/Markdown/32_001_alltrainable_0.md" -J "32_001_alltrainable_0" -env MYARGS="-name 32_001_alltrainable-0 -time 3600 -lr 0.01 -batch_size 32 -steps 5000 -ID 0" < submit_gpu_a80.sh
+mkdir -p outputs/32_0001_alltrainable/Markdown
+bsub -o "outputs/32_0001_alltrainable/Markdown/32_0001_alltrainable_0.md" -J "32_0001_alltrainable_0" -env MYARGS="-name 32_0001_alltrainable-0 -time 3600 -lr 0.001 -batch_size 32 -steps 5000 -ID 0" < submit_gpu_a80.sh
