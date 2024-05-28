2d53b95cee9cc9ef944df4bdc72be147e8b4b7da
TheDingodile <jakob@3esser.dk>
2024-05-28 14:22:06 +0900

    123

--- experiments.sh
+++ experiments.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mkdir -p outputs/a80testa80/Markdown
-bsub -o "outputs/a80testa80/Markdown/a80testa80_0.md" -J "a80testa80_0" -env MYARGS="-name a80testa80-0 -time 3600 -lr 0.01 -batch_size 16 -steps 5000 -ID 0" < submit_gpu_a80.sh
+mkdir -p outputs/v32test8/Markdown
+bsub -o "outputs/v32test8/Markdown/v32test8_0.md" -J "v32test8_0" -env MYARGS="-name v32test8-0 -time 3600 -lr 0.01 -batch_size 8 -steps 5000 -ID 0" < submit_gpu_v32.sh
