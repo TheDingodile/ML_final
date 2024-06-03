b580a824ddfe49444547b150ada4e2b58dd8200c
TheDingodile <jakob@3esser.dk>
2024-06-03 23:20:06 +0900

    123

--- experiments.sh
+++ experiments.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mkdir -p outputs/eval_full/Markdown
-bsub -o "outputs/eval_full/Markdown/eval_full_0.md" -J "eval_full_0" -env MYARGS="-name eval_full-0 -time 3600 -vqa_module_type yes -ID 0" < submit_gpu_a80.sh
+mkdir -p outputs/eval_fulla40/Markdown
+bsub -o "outputs/eval_fulla40/Markdown/eval_fulla40_0.md" -J "eval_fulla40_0" -env MYARGS="-name eval_fulla40-0 -time 3600 -vqa_module_type custom -ID 0" < submit_gpu_a40.sh
