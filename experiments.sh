596f4effc9f0d91db095e06521bb3ef02a331686
TheDingodile <jakob@3esser.dk>
2024-06-04 13:39:58 +0900

    123

--- experiments.sh
+++ experiments.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mkdir -p outputs/True_SQL_evala80/Markdown
-bsub -o "outputs/True_SQL_evala80/Markdown/True_SQL_evala80_0.md" -J "True_SQL_evala80_0" -env MYARGS="-name True_SQL_evala80-0 -time 3600 -vqa_module_type custom -ID 0" < submit_gpu_a80.sh
+mkdir -p outputs/True_SQL_test/Markdown
+bsub -o "outputs/True_SQL_test/Markdown/True_SQL_test_0.md" -J "True_SQL_test_0" -env MYARGS="-name True_SQL_test-0 -time 3600 -vqa_module_type custom -ID 0" < submit_gpu_v32.sh
